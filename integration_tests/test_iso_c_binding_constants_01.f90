d51aca10cad6a61773780aede6e16dc515c816d2
nilocj <nilocj@outlook.com>
2026-02-16 08:59:41 -0300

    iso_c_const

--- integration_tests/test_iso_c_binding_constants_01.f90
+++ integration_tests/test_iso_c_binding_constants_01.f90
@@ -1,34 +1,25 @@
 module iso_c_const
+
   implicit none
+ 
+  type :: c_ptr
+    integer ptr
+  end type
 
-  enum, bind(c)
-   enumerator :: c_int = 4, c_short = 2, c_long = 8, c_long_long = 8
-  end enum
+  type :: c_funptr
+    integer ptr
+  end type
 
-  enum, bind(c)
-   enumerator :: c_signed_char = 1, c_size_t = 8
-  end enum
-
-  enum, bind(c)
-   enumerator :: c_int8_t = 1, c_int16_t = 2, c_int32_t = 4, c_int64_t = 8
-   enumerator :: c_int8_t = 1, c_int16_t = 2, c_int32_t = 4, c_int64_t = 8
-   enumerator :: c_int_least8_t = 1, c_int_least16_t = 2, c_int_least32_t = 4, c_int_least64_t = 8
-   enumerator :: c_int_fast8_t = 1, c_int_fast16_t = 2, c_int_fast32_t = 4, c_int_fast64_t = 8
-  end enum
-
-  enum, bind(c)
-   enumerator :: c_intmax_t = 8
-   enumarator :: c_intptr_t = 8, c_ptrdiff_t = 8
-  end enum
-
-  enum, bind(c)
-   enumerator :: c_float = 4, c_double = 8, c_double_complex = 8
-   enumerator :: c_long_double = -1, c_long_double_complex = -1
-  end enum
-
-  enum, bind(c)
-   enumerator :: c_bool = 1, c_char = 1
-  end enum
+  integer, parameter :: c_int = 4, c_short = 2, c_long = 8, c_long_long = 8
+  integer, parameter :: c_signed_char = 1, c_size_t = 8
+  integer, parameter :: c_int8_t = 1, c_int16_t = 2, c_int32_t = 4, c_int64_t = 8
+  integer, parameter :: c_int_least8_t = 1, c_int_least16_t = 2, c_int_least32_t = 4, c_int_least64_t = 8
+  integer, parameter :: c_int_fast8_t = 1, c_int_fast16_t = 2, c_int_fast32_t = 4, c_int_fast64_t = 8
+  integer, parameter :: c_intmax_t = 8
+  integer, parameter :: c_intptr_t = 8, c_ptrdiff_t = 8
+  integer, parameter :: c_float = 4, c_double = 8, c_float_complex=8, c_double_complex = 8 
+  integer, parameter :: c_long_double = -1, c_long_double_complex = -1
+  integer, parameter :: c_bool = 1, c_char = 1
 
   character(len=1), parameter :: c_null_char = char(0)
   character(len=1), parameter :: c_alert = char(7)
@@ -39,17 +30,55 @@ module iso_c_const
   character(len=1), parameter :: c_horizontal_tab = char(9)
   character(len=1), parameter :: c_vertical_tab = char(11)
   
+  type(c_ptr), parameter :: c_null_ptr = c_ptr(0)
+  type(c_funptr), parameter :: c_null_funptr = c_funptr(0)
+
+  interface
+    logical function c_associated(c_ptr_1)
+    import c_ptr
+    type(c_ptr), intent(in) :: c_ptr_1
+    end function
+
+    subroutine c_f_pointer(cptr, fptr, shape)
+    import c_ptr
+    type(c_ptr), intent(in) :: cptr
+    !type(*), pointer, intent(out) :: fptr
+    integer, pointer, intent(out) :: fptr
+    integer, intent(in), optional :: shape(:)
+    end subroutine
+
+    subroutine c_f_procpointer(cptr, fptr)
+    import c_funptr
+    type(c_funptr), intent(in) :: cptr
+    !procedure(*), pointer, intent(out) :: fptr
+    integer, pointer, intent(out) :: fptr
+    end subroutine
+
+    !type(c_ptr) function c_loc(x)
+    integer function c_loc(x)
+    import c_ptr
+    !type(*), intent(in) :: x
+    integer, intent(in) :: x
+    end function
+
+    !type(c_funptr) function c_funloc(x)
+    integer function c_funloc(x)
+    import c_funptr
+    !type(*), intent(in) :: x
+    integer, intent(in) :: x
+    end function
+  end interface
+
 end module iso_c_const
 
-program test_iso_c_binding_constants_02
-   use iso_c_const
-   use iso_c_binding
+program test_iso_c_binding_constants_01
+  use iso_c_const
 
-   print *, "c_int = ", c_int
-   if (c_int /= 4) error stop
+  print *, "c_int = ", c_int
+  if (c_int /= 4) error stop
    
-   print *, "c_short = ", c_short
-   if (c_short /= 2) error stop
+  print *, "c_short = ", c_short
+  if (c_short /= 2) error stop
    
    print *, "c_long = ", c_long
    if (c_long /= 8) error stop
@@ -156,5 +185,5 @@ program test_iso_c_binding_constants_02
    
    print *, "c_vertical_tab = ", c_vertical_tab
    if (c_vertical_tab /= char(11)) error stop
-
+    
 end program test_iso_c_binding_constants_01
