dc9d22c0d0200f3da52c23714f5cc18933b5b6c7
nilocj <nilocj@outlook.com>
2026-02-19 15:56:00 -0300

    pixici test

--- ci/pixici/reset.sh
+++ ci/pixici/reset.sh
@@ -2,16 +2,11 @@
 
 set -ex
 
-export LFORTRAN_CMAKE_GENERATOR=Ninja
-
 echo "Running SHELL"
 echo "CONDA_PREFIX=$CONDA_PREFIX"
-echo "LFORTRAN_CMAKE=$LFORTRAN_CMAKE_GENERATOR"
 
-rm -rf ../lfortran && mkdir -p ../lfortran && cp pixi.toml ../lfortran/pixi.toml
-cp ../../setvars.* .
-cp setvars.* ../lfortran
-cd ../lfortran
+cp ../lfortran/pixi.toml pixi.toml.bak && cp setvars.* ../lfortran
+cp pixi.toml.blank ../lfortran/pixi.toml && cd ../lfortran
 
 pixi import ../environment_linux.yml -p linux-64 -f unix
 pixi add -p linux-64 -f unix libunwind=1.7.2
@@ -20,10 +15,10 @@ pixi import ../../environment_win.yml -p win-64 -f windows
 ../version.sh && pixi workspace version set $(cat version)
 
 pixi workspace description set "Modern interactive LLVM-based Fortran compiler"
+pixi workspace platform add win-64 osx-64 osx-arm64
 
-pixi task add clean "git clean -dfx -e '.pixi'" -p linux-64 -f unix
-
-pixi task add ci "./ci/build.sh" --cwd ../../ --depends-on clean -p linux-64 -f unix
+pixi task add ci "./ci/build.sh" --cwd ../../ -p linux-64 -f unix
 
 pixi update
 
+echo "Change to directory $(pwd) and execute: <pixi run -e unix ci> "
