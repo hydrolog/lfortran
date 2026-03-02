d2e71997a3d15b7fe8832786532d84e4ffc18b55
Valdenor Nilo de Carvalho Junior <nilocj@outlook.com>
2026-03-02 16:21:52 -0300

    Build for windows

--- build1.bat
+++ build1.bat
@@ -1,10 +1,15 @@
-cmake ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DWITH_LLVM=yes ^
-    -DLFORTRAN_BUILD_ALL=yes ^
-    -DWITH_STACKTRACE=no ^
-    -DCMAKE_PREFIX_PATH="%CONDA_PREFIX%" ^
-    -DCMAKE_INSTALL_PREFIX=%cd%/inst ^
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=yes ^
-    .
-cmake --build . --config Release --target install
+del CMakeCache.txt
+
+cmake -S %cd% -B %cd% -G Ninja ^
+ -DCMAKE_BUILD_TYPE=Release ^
+ -DWITH_LLVM=yes ^
+ -DLFORTRAN_BUILD_ALL=no ^
+ -DWITH_STACKTRACE=no ^
+ -DWITH_RUNTIME_STACKTRACE=no ^
+ -DCMAKE_PREFIX_PATH="%CONDA_PREFIX%" ^
+ -DCMAKE_INSTALL_PREFIX="%CONDA_PREFIX%\Library" ^
+ -DCMAKE_EXPORT_COMPILE_COMMANDS=yes
+cmake --build %cd% --target install
+
+
+
