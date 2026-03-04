bd6edd153896f7ecd42ac6aa133c57cfe1f33e69
nilocj <nilocj@outlook.com>
2026-03-04 12:39:25 -0300

    Out of source build

--- build_release.sh
+++ build_release.sh
@@ -3,19 +3,22 @@
 set -e
 set -x
 
-cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_FLAGS_RELEASE="-Wall -Wextra -O3 -funroll-loops -DNDEBUG" \
-    -DWITH_LLVM=yes \
-    -DWITH_LSP=yes \
-    -DLFORTRAN_BUILD_ALL=yes \
-    -DWITH_STACKTRACE=no \
-    -DWITH_RUNTIME_STACKTRACE=yes \
-    -DUSE_DYNAMIC_ZSTD=no \
-    -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH_LFORTRAN;$CONDA_PREFIX" \
-    -DCMAKE_INSTALL_PREFIX=`pwd`/inst \
-    -DCMAKE_INSTALL_LIBDIR=share/lfortran/lib \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=yes \
-    -G Ninja \
-    .
-cmake --build . --target install
+BUILD_DIR=${BUILD_DIR:-build}
+INSTALL_DIR=${INSTALL_DIR:-${CONDA_PREFIX}}
+
+cmake -S . -B ${BUILD_DIR} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_FLAGS_RELEASE="-Wall -Wextra -O3 -funroll-loops -DNDEBUG" \
+  -DWITH_LLVM=yes \
+  -DWITH_LSP=yes \
+  -DLFORTRAN_BUILD_ALL=yes \
+  -DWITH_STACKTRACE=no \
+  -DWITH_RUNTIME_STACKTRACE=yes \
+  -DUSE_DYNAMIC_ZSTD=no \
+  -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH_LFORTRAN;$CONDA_PREFIX" \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+  -DCMAKE_INSTALL_LIBDIR=share/lfortran/lib \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=yes \
+  -G Ninja
+
+cmake --build ${BUILD_DIR} --target install
